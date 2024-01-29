0cf4994f3c4631f9042bb2d317f05051c3d08ce8
nuhin13 <nuhin13@gmail.com>
2024-01-29 18:06:31 +0600

    [Refactor] api service

--- data/src/main/java/com/nuhin13/data/api/ApiService.kt
+++ data/src/main/java/com/nuhin13/data/api/ApiService.kt
@@ -3,6 +3,4 @@ package com.nuhin13.data.api
 import com.nuhin13.data.features.post.datasource.PostApiService
 import com.nuhin13.data.features.user.datasource.UserApiService
 
-interface ApiService: PostApiService, UserApiService {
-
-}
+interface ApiService: PostApiService, UserApiService

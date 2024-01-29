67b0d33feb9f942341422dda2d2b12dc89424448
nuhin13 <nuhin13@gmail.com>
2024-01-29 15:48:57 +0600

    [CLEAN] api service

--- data/src/main/java/com/nuhin13/data/api/ApiService.kt
+++ data/src/main/java/com/nuhin13/data/api/ApiService.kt
@@ -1,12 +1,7 @@
 package com.nuhin13.data.api
 
 import com.nuhin13.data.features.post.datasource.PostApiService
-import com.nuhin13.data.features.post.dtos.PostApiResponse
-import com.nuhin13.data.features.post.dtos.PostCommentApiResponse
 import com.nuhin13.data.features.user.datasource.UserApiService
-import com.nuhin13.data.features.user.dtos.UserApiResponse
-import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface ApiService: PostApiService, UserApiService {
 

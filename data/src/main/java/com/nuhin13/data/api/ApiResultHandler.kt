8ff7d8312c620ee962ca8742dee18c251671294d
nuhin13 <nuhin13@gmail.com>
2024-02-04 20:21:22 +0600

    [ADD] api data manipulation classes

---
+++ data/src/main/java/com/nuhin13/data/api/ApiResultHandler.kt
@@ -0,0 +1,26 @@
+package com.nuhin13.data.api
+
+import androidx.lifecycle.MutableLiveData
+
+class ApiResultHandler<T>(private val onSuccess: (T?) -> Unit, private val onFailure: (T?) -> Unit)  {
+
+    var loading = MutableLiveData<Boolean>()
+
+    fun handleApiResult(result: ApiResult<T?>) {
+        when (result.status) {
+            ApiStatus.LOADING -> {
+                loading.value = true
+            }
+
+            ApiStatus.SUCCESS -> {
+                loading.value = false
+                onSuccess(result.data)
+            }
+
+            ApiStatus.ERROR -> {
+                loading.value = false
+                onFailure(result.data)
+            }
+        }
+    }
+}
